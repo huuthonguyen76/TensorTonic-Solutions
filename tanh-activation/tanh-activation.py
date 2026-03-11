bb1a2d1ff6a9d0066c7d0f70bc2e33d3b75b9417
Huu Tho Nguyen (Barney) <huu.thonguyen76@gmail.com>
2026-03-11 18:18:35 +0700

    Add solution for tanh-activation

---
+++ tanh-activation/tanh-activation.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+
+
+def tanh(x):
+    """
+    Implement Tanh activation function.
+    """
+    # Write code here
+    x = np.asarray(x)
+    e = math.e
+    def _tanh(x):
+        return (e ** x - (e ** (-x))) / ((e ** x) + (e ** (-x)))
+    return _tanh(x)
